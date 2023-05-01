3e259724ef51bcd8d363a5a33a2135e803276e8e
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-05-01 19:47:51 +0200

    Initial Commit

---
+++ LocalNotifications/LocalNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsApp.swift
+//  LocalNotifications
+//
+//  Created by Tobias Sörensson on 2023-05-01.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
